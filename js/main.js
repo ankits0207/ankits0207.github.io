5a611d194935b1b80a2030ae1da3790a1857422b
Ankit Sharma <ankitsharma0207@gmail.com>
2019-05-04 23:06:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -135,6 +135,24 @@ var main = {
   }
 };
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
