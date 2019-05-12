29d813c9e4be6430f3a82990ce0b60fe21928063
Ankit Sharma <ankitsharma0207@gmail.com>
2019-05-12 12:37:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = "block";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
