15572a53121dc43876a0cc707a24d91ea2345d5d
Ankit Sharma <ankitsharma0207@gmail.com>
2019-05-12 11:32:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,5 +136,20 @@ var main = {
 };
 
 // 2fc73a3a967e97599c9763d05e564189#
+// Carousel
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
 					      
 document.addEventListener('DOMContentLoaded', main.init);
