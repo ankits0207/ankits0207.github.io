c4919b76d277f8d0dfe8b464e336b7f4fb7139dc
Ankit Sharma <ankitsharma0207@gmail.com>
2020-02-24 14:51:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -153,3 +153,23 @@ function carousel() {
 }
 					      
 document.addEventListener('DOMContentLoaded', main.init);
+
+/*Black skill bar*/
+var php = function(){
+	document.querySelector(".php").className = "php1";
+};
+var css = function(){
+	document.querySelector(".css").className = "css1";
+};
+var htm = function(){
+document.querySelector(".htm").className = "htm1";	
+};
+var jav = function(){
+document.querySelector(".javascript").className = "javascript1";	
+};
+setTimeout(php,1000);
+setTimeout(css,3000);
+setTimeout(htm,5000);
+setTimeout(jav,7000);
+
+
